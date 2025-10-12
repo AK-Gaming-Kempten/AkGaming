6d09b19ef5fc70eee202b13c6e096031d958b4e8
hexasiel <hexasiel@gmail.com>
2025-10-12 22:04:33 +0200

    Minor change to home page

--- apps/akgaming_website/src/pages/Home.tsx
+++ apps/akgaming_website/src/pages/Home.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
     return (
         <main>
-            <h1>Welcome to the Homepage</h1>
-            <p>This is a static website built with Vite + React.</p>
+            <h1>In Arbeit...</h1>
         </main>
     );
 }
