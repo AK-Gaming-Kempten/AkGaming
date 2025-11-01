8d066b60fcbc5f6e20a4b42ccd1e436af27f3138
hexasiel <hexasiel@gmail.com>
2025-11-01 13:51:58 +0100

    Add role mapping to web api

--- apps/Management-Tool/src/ManagementTool.WebApi/Program.cs
+++ apps/Management-Tool/src/ManagementTool.WebApi/Program.cs
@@ -1,6 +1,8 @@
+using System.Security.Claims;
 using System.Text.Json.Serialization;
 using MemberManagement.Api;
 using MemberManagement.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -25,6 +27,24 @@ builder.Services
         option.TokenValidationParameters = new TokenValidationParameters {
             NameClaimType = "preferred_username",
             RoleClaimType = "roles"
+        };
+        option.Events = new JwtBearerEvents {
+            OnTokenValidated = context => {
+                var claims = context.Principal!.Claims.ToList();
+                var identity = (ClaimsIdentity)context.Principal.Identity!;
+
+                // Extract roles from realm_access.roles[]
+                var realmRoles = context.Principal.FindAll("realm_access.roles");
+                foreach (var r in realmRoles)
+                    identity.AddClaim(new Claim(ClaimTypes.Role, r.Value));
+
+                // Extract roles from resource_access.<client>.roles[]
+                var resourceRoles = context.Principal.FindAll("resource_access.managementtool-api.roles");
+                foreach (var r in resourceRoles)
+                    identity.AddClaim(new Claim(ClaimTypes.Role, r.Value));
+
+                return Task.CompletedTask;
+            }
         };
     });
 
