528b0c969ac44a1f552106b17d02d781fd29834b
hexasiel <hexasiel@gmail.com>
2026-07-22 18:03:19 +0200

    [Management] Fix error in Board management DB migration action

--- AkGaming.Management/Modules/BoardManagement/Migrations/Postgres/BoardManagementDbContextPostgresFactory.cs
+++ AkGaming.Management/Modules/BoardManagement/Migrations/Postgres/BoardManagementDbContextPostgresFactory.cs
@@ -8,7 +8,13 @@ public sealed class BoardManagementDbContextPostgresFactory : IDesignTimeDbConte
 {
     public BoardManagementDbContext CreateDbContext(string[] args)
     {
-        var options = new DbContextOptionsBuilder<BoardManagementDbContext>().UseNpgsql("Host=localhost;Database=board_design;Username=postgres;Password=postgres", database => database.MigrationsAssembly(typeof(BoardManagementDbContextPostgresFactory).Assembly.FullName)).Options;
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+            ?? "Host=localhost;Database=board_design;Username=postgres;Password=postgres";
+        var options = new DbContextOptionsBuilder<BoardManagementDbContext>()
+            .UseNpgsql(
+                connectionString,
+                database => database.MigrationsAssembly(typeof(BoardManagementDbContextPostgresFactory).Assembly.FullName))
+            .Options;
         return new BoardManagementDbContext(options);
     }
 }
